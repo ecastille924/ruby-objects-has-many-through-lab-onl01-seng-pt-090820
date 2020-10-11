4d835da183ed88730c0ae05a1b74179fd0340e9f
Erick Castille <ecastille924@gmail.com>
2020-10-11 19:51:50 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,4 +1,4 @@
-require "pry"
+# require "pry"
 
 class Doctor
 
@@ -19,7 +19,6 @@ class Doctor
   def patients
     appointments.map do |appointment|
       appointment.patient
-      binding.pry
     end
   end
 
