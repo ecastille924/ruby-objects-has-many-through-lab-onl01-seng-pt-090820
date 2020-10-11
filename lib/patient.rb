a41509c5d4708d148a04b0c024b29e1d58eb4b5a
Erick Castille <ecastille924@gmail.com>
2020-10-11 21:07:27 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,6 +12,10 @@ class Patient
     @@all
   end
 
+  def new_appointment(date, doctor)
+    Appointment.new(self, date, doctor)
+  end
+  
   def appointments
     Appointment.all.select do |appointment|
       appointment.patient == self
@@ -22,10 +26,6 @@ class Patient
     Appointment.all.map do |appointment| 
       appointment.doctor
     end
-  end
-
-  def new_appointment(date, doctor)
-    Appointment.new(self, date, doctor)
   end
 
 end 
